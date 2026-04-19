7c8ac5ea3047e43ca261840988c34e9109836f76
bakingfrenchie <brion.nicholas@gmail.com>
2026-04-19 17:41:42 -0400

    Create settings_menu.gd
    Created logic for settings menu

---
+++ GameFiles/project-seasons/scripts/settings_menu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	custom_minimum_size = get_viewport_rect().size
+	mouse_filter = Control.MOUSE_FILTER_STOP
+	$Panel/VBoxContainer/CloseSetting.pressed.connect(_on_closeSetting_button_pressed)
+	$Panel/VBoxContainer/HomeScreen.pressed.connect(_on_homeScreen_button_pressed)
+	$Panel/VBoxContainer/Quit.pressed.connect(_on_quit_button_pressed)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _on_closeSetting_button_pressed():
+	queue_free()
+
+func _on_homeScreen_button_pressed():
+	get_tree().change_scene_to_file("res://title_screen.tscn")
+
+func _on_quit_button_pressed():
+	get_tree().quit()
